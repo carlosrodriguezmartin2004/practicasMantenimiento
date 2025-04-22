7732df48a7137325e1cccb507ed812425116f07b
Carlos Rodríguez <carlosrodriguezmartin2004@gmail.com>
2025-04-22 21:00:51 +0200

    Avance

---
+++ practica4/src/test/java/org/mps/TournamentSelectionTest.java
@@ -0,0 +1,28 @@
+package org.mps;
+
+import org.junit.jupiter.api.Test;
+import org.mps.selection.TournamentSelection;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TournamentSelectionTest {
+
+    @Test
+    public void tournamentSelection_tamanoMenor0_throwEvolutionaryAlgorithmException() {
+        int tamano = -1;
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> {
+            new TournamentSelection(tamano);
+        });
+    }
+
+    public void tournamentSelection_tamanoMayor0_True() {
+        int tamano = 1;
+
+        assertDoesNotThrow(EvolutionaryAlgorithmException.class, () -> {
+            new TournamentSelection(tamano);
+        });
+    }
+}
+
