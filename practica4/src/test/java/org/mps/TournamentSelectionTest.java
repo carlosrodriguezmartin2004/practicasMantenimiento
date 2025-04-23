a124b5fec4848ed5f5fb3edd93f8271837cff1fb
Carlos Rodríguez <carlosrodriguezmartin2004@gmail.com>
2025-04-23 15:12:17 +0200

    TestTournamentSelection

--- practica4/src/test/java/org/mps/TournamentSelectionTest.java
+++ practica4/src/test/java/org/mps/TournamentSelectionTest.java
@@ -1,6 +1,8 @@
 package org.mps;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 import org.mps.selection.TournamentSelection;
 
 
@@ -9,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TournamentSelectionTest {
 
     @Test
+    @DisplayName("Tournament con tamanao menor a 0")
     public void tournamentSelection_tamanoMenor0_throwEvolutionaryAlgorithmException() {
         int tamano = -1;
 
@@ -17,12 +20,67 @@ public class TournamentSelectionTest {
         });
     }
 
-    public void tournamentSelection_tamanoMayor0_True() {
+    @Test
+    @DisplayName("Tournament con tamanao mayor a 0")
+    public void tournamentSelection_tamanoMayor0_True() throws EvolutionaryAlgorithmException {
         int tamano = 1;
 
-        //assertDoesNotThrow(EvolutionaryAlgorithmException.class, () -> {
-         //   new TournamentSelection(tamano);
-        //});
+        TournamentSelection t = new TournamentSelection(tamano);
+
+        assertNotNull(t);
     }
+
+    @Test
+    @DisplayName("Population es Null")
+    public void select_PopulationNull_ThrowEvolutionaryAlgorithmException() throws EvolutionaryAlgorithmException {
+        int tamano = 1;
+        int [] population = null;
+
+        TournamentSelection t = new TournamentSelection(tamano);
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> {
+            t.select(population);
+        });
+    }
+
+    @Test
+    @DisplayName("Tamano  de Population Menor que Tamano")
+    public void select_TamanoPopulationMenorQueTamano_ThrowEvolutionaryAlgorithmException() throws EvolutionaryAlgorithmException {
+        int tamano = 10;
+        int [] population = {1,2,3};
+
+        TournamentSelection t = new TournamentSelection(tamano);
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> {
+            t.select(population);
+        });
+    }
+
+    @Test
+    @DisplayName("Tamano de Population Menor que 1")
+    public void select_TamanoPopulationMenorQue1_ThrowEvolutionaryAlgorithmException() throws EvolutionaryAlgorithmException {
+        int tamano = 1;
+        int [] population = {};
+
+        TournamentSelection t = new TournamentSelection(tamano);
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> {
+            t.select(population);
+        });
+    }
+
+    @Test
+    @DisplayName("Correcto")
+    public void select_Correcto_Return() throws EvolutionaryAlgorithmException {
+        int tamano = 5;
+        int [] population = {5,4,3,2,1,6};
+
+        TournamentSelection t = new TournamentSelection(tamano);
+        int[] actual = t.select(population);
+
+        assertEquals(population.length, actual.length);
+    }
+
+
 }
 
