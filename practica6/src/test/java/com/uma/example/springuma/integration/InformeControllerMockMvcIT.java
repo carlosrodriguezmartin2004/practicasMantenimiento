bdd9b4a44ab61fae099b187031bee73ba5d4eb61
joseruuiz4 <joseruizpareja@uma.es>
2025-05-21 13:29:32 +0200

    empezado informeTests

--- practica6/src/test/java/com/uma/example/springuma/integration/InformeControllerMockMvcIT.java
+++ practica6/src/test/java/com/uma/example/springuma/integration/InformeControllerMockMvcIT.java
@@ -1,4 +1,37 @@
 package com.uma.example.springuma.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uma.example.springuma.model.Informe;
+import jakarta.annotation.PostConstruct;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Duration;
+
 public class InformeControllerMockMvcIT {
+    @LocalServerPort
+    private int port;
+    private WebTestClient client;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostConstruct
+    public void init() {
+        client = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .responseTimeout(Duration.ofSeconds(10))
+                .build();
+    }
+
+
+    @Test
+    @DisplayName("crearInforme crea correctamente un informe y comprobamos que se haya creado")
+    void crearInforme_Correct(){
+        Informe informe = new Informe();
+    }
+
+
 }
